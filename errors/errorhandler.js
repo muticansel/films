4d74ad3eaf05eaba0dde894f88d7fdabbfc5050d
muti <cancel.muti@gmail.com>
2020-01-27 20:55:53 +0100

    firebase signup & login errors extra

--- errors/errorhandler.js
+++ errors/errorhandler.js
@@ -1,16 +1,26 @@
 const INVALID_PASSWORD = "INVALID_PASSWORD";
+const INVALID_EMAIL = "INVALID_EMAIL";
 const EMAIL_NOT_FOUND = "EMAIL_NOT_FOUND";
 const EMAIL_EXISTS = "EMAIL_EXISTS";
-
+const MISSING_EMAIL = "MISSING_EMAIL";
+const MISSING_PASSWORD = "MISSING_PASSWORD";
 
 export const firebaseAuthErrorHandlig = (errData) => {
     switch (errData.error.message) {
         case INVALID_PASSWORD:
-            return "Your password is wrong. Please try again!"
+            return "Your password is wrong. Please try again!";
+        case INVALID_EMAIL:
+            return "Your have to enter a valid e-mail!"
         case EMAIL_NOT_FOUND:
             return "This email could not be found. Please sign up to be able to login";
-            case EMAIL_EXISTS:
-                    return "This email exists in our system. Please try another one.";
+        case EMAIL_EXISTS:
+            return "This email exists in our system. Please try another one.";
+        case EMAIL_EXISTS:
+            return "This email exists in our system. Please try another one.";
+        case MISSING_EMAIL:
+            return "I guess, you forgot to enter the e-mail. Are you here?";
+        case MISSING_PASSWORD:
+            return "I guess, you forgot to enter the password. Do you know it is necessary?";
         default:
             return errData.error.message;
     }
