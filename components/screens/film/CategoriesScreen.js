41f4c226c537d95cd095fe214ec9a18bd91a9045
muti <cancel.muti@gmail.com>
2020-01-30 11:52:13 +0100

    fetchFilms in the category page

--- components/screens/film/CategoriesScreen.js
+++ components/screens/film/CategoriesScreen.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import { StyleSheet, FlatList, View, Text, TouchableOpacity, Platform } from 'react-native';
+import React, { useEffect, useState, useCallback } from 'react';
+import { StyleSheet, FlatList, ActivityIndicator, View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux'
 
 import { CATEGORIES } from '../../../constants/categories';
 import CategoryGrid from '../../UI/CategoryGrid';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-
 import CustomHeaderButton from '../../UI/HeaderButton';
-
+import DefaultText from '../../UI/DefaultText';
+import * as filmActions from '../../../store/actions/films'
+import Colors from '../../../constants/colors';
 
 
 const CategoriesScreen = props => {
+    const [isLoading, setIsLoading] = useState(false);
+    const films = useSelector(state => state.filmReducer.films);
+    const dispatch = useDispatch()
+
+    const loadFilms = useCallback(async () => {
+        setIsLoading(true);
+        await dispatch(filmActions.fetchFilms())
+        setIsLoading(false);
+    }, [dispatch, setIsLoading])
+
+    useEffect(() => {
+        loadFilms()
+    }, [dispatch, loadFilms])
+
+    useEffect(() => {
+        const willFocusSub = props.navigation.addListener('willFocus', loadFilms);
+
+        return () => {
+            willFocusSub.remove()
+        }
+    }, [loadFilms])
+
     const renderGridItem = itemData => {
         return (
             <CategoryGrid title={itemData.item.title}
@@ -17,6 +41,22 @@ const CategoriesScreen = props => {
                 onSelect={() => props.navigation.navigate('CategoryFilms', {
                     categoryId: itemData.item.id
                 })} />
+        )
+    }
+
+    if (isLoading) {
+        return (
+            <View style={styles.loading}>
+                <ActivityIndicator size="large" color={Colors.primary} />
+            </View>
+        )
+    }
+
+    if (!isLoading && films.length === 0) {
+        return (
+            <View style={styles.loading}>
+                <DefaultText>No film is found. You can start to add...</DefaultText>
+            </View>
         )
     }
 
@@ -41,6 +81,11 @@ CategoriesScreen.navigationOptions = navData => {
 }
 
 const styles = StyleSheet.create({
+    loading: {
+        flex: 1,
+        justifyContent: 'center',
+        alignContent: 'center'
+    }
 })
 
 export default CategoriesScreen;
